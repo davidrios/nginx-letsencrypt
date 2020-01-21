a7689ef749c54f7bbfe9a77a7f303b32de2e868b
David Rios <david.rios.gomes@gmail.com>
2020-01-21 17:20:56 -0300

    Add files

---
+++ nginx/nginx-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+trap '[ -z "$child" ] || kill -INT $child' EXIT
+trap '[ -z "$child" ] || kill -TERM $child' TERM
+trap '[ -z "$child" ] || kill -INT $child' INT
+
+mkdir /var/www/letsencrypt
+
+nginx -g "daemon off;" &
+child=$!
+
+[ -d "/etc/letsencrypt/live/test.davidrios.dev" ] || certbot certonly -n --webroot -w /var/www/letsencrypt --cert-name test.davidrios.dev --agree-tos --email david.rios.gomes@gmail.com -d test.davidrios.dev
+certbot renew
+
+cp /root/test.conf /etc/nginx/conf.d
+nginx -s reload
+wait "$child"
